95b9f760fff323541b37d6c022dd12796d637929
sharath kumar <sharathdhd@gmail.com>
2024-06-29 05:10:56 +0530

    Update app_01.py

--- app_01.py
+++ app_01.py
@@ -23,7 +23,10 @@ class FileManager:
         """
         try:
             if self.uploaded_file:
-                return self.uploaded_file.read().decode('utf-8')
+                data = self.uploaded_file.read().decode('utf-8')
+                logging.info("Data read from uploaded file.")
+                return data
+            logging.info("Data read from text area.")
             return self.data
         except Exception as e:
             logging.error(f"Error reading data: {e}")
@@ -49,6 +52,7 @@ class FileManager:
         
         if current_file_name:
             self.files[current_file_name] = current_file_content
+        logging.info("Data parsed into files.")
 
     def get_file_list(self):
         """
@@ -72,9 +76,33 @@ class FileManager:
                 for file_name in selected_files:
                     zipf.writestr(file_name, self.files[file_name])
             self.in_memory_zip.seek(0)
+            logging.info("Zip file created in memory.")
         except Exception as e:
             logging.error(f"Error creating zip file: {e}")
             st.error("Error creating zip file")
+
+def display_file_options(file_manager):
+    """
+    Display options for selecting files to include in the zip.
+    """
+    file_types = file_manager.get_file_types()
+    file_list = file_manager.get_file_list()
+    st.write(f"Files that can be created: {file_list}")
+
+    # Select all files checkbox
+    if st.checkbox('Select All Files'):
+        selected_files = file_list
+    else:
+        selected_files = []
+
+    # Select files by type checkboxes
+    for file_type in file_types:
+        if st.checkbox(f'Select all .{file_type} files'):
+            selected_files.extend([file_name for file_name in file_list if file_name.endswith(f'.{file_type}')])
+
+    selected_files = list(set(selected_files))  # Remove duplicates
+    selected_files_display = st.multiselect('Or select individual files:', file_list, default=selected_files)
+    return selected_files_display
 
 def main():
     """
@@ -90,43 +118,26 @@ def main():
         # Initialize FileManager and process the data
         file_manager = FileManager(data, uploaded_file)
         raw_data = file_manager.read_data()
-        file_manager.parse_data(raw_data)
+        if raw_data:
+            file_manager.parse_data(raw_data)
+            selected_files_display = display_file_options(file_manager)
 
-        file_types = file_manager.get_file_types()
-        file_list = file_manager.get_file_list()
+            # Input for custom zip file name
+            zip_file_name = st.text_input('Enter a name for the zip file (optional):')
+            if zip_file_name:
+                if not zip_file_name.lower().endswith('.zip'):
+                    zip_file_name += '.zip'
+            else:
+                zip_file_name = f'generated_zip_{int(time.time())}.zip'
 
-        st.write(f"Files that can be created: {file_list}")
-
-        # Select all files checkbox
-        if st.checkbox('Select All Files'):
-            selected_files = file_list
-        else:
-            selected_files = []
-
-        # Select files by type checkboxes
-        for file_type in file_types:
-            if st.checkbox(f'Select all .{file_type} files'):
-                selected_files.extend([file_name for file_name in file_list if file_name.endswith(f'.{file_type}')])
-
-        selected_files = list(set(selected_files))  # Remove duplicates
-        selected_files_display = st.multiselect('Or select individual files:', file_list, default=selected_files)
-
-        # Input for custom zip file name
-        zip_file_name = st.text_input('Enter a name for the zip file (optional):')
-        if zip_file_name:
-            if not zip_file_name.lower().endswith('.zip'):
-                zip_file_name += '.zip'
-        else:
-            zip_file_name = f'generated_zip_{int(time.time())}.zip'
-
-        if selected_files_display:
-            file_manager.create_zip_in_memory(selected_files_display)
-            st.download_button(
-                label="Download Zip File",
-                data=file_manager.in_memory_zip,
-                file_name=zip_file_name,
-                mime="application/zip"
-            )
+            if selected_files_display:
+                file_manager.create_zip_in_memory(selected_files_display)
+                st.download_button(
+                    label="Download Zip File",
+                    data=file_manager.in_memory_zip,
+                    file_name=zip_file_name,
+                    mime="application/zip"
+                )
 
 if __name__ == "__main__":
     main()
